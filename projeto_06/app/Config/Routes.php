695e297cf7d5d50c8d9a6dd8f08db37dc9c1cde6
Felipe Pinheiro <santospinheiro627@gmail.com>
2024-01-21 16:53:09 -0300

    views and controllers

--- projeto_06/app/Config/Routes.php
+++ projeto_06/app/Config/Routes.php
@@ -5,6 +5,23 @@ use CodeIgniter\Router\RouteCollection;
 /*
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
 
-$routes->get('/adicao/');
+// MathController
+$routes->get('adicao/(:num)/(:num)', 'MathController::add/$1/$2');
+$routes->get('subtracao/(:num)/(:num)', 'MathController::sub/$1/$2');
+$routes->get('multiplicacao/(:num)/(:num)', 'MathController::mult/$1/$2');
+$routes->get('divisao/(:num)/(:num)', 'MathController::divide/$1/$2');
+
+// TalkController
+$routes->get('palavra/(:alpha)', 'TalkController::word/$1');
+$routes->get('palavras/(:alpha)/(:alpha)', 'TalkController::words/$1/$2');
+
+$routes->get('outro/(:alpha)', 'TalkController::other/$1');
+$routes->get('outro', 'TalkController::other'); // route with default value
+
+// controller/view/model
+
+// view route
+$routes->view('/', 'index');
+
+$routes->view('pagina1/(:any)', 'index');
--- projeto_06/app/Controllers/TalkController.php
+++ projeto_06/app/Controllers/TalkController.php
@@ -2,13 +2,26 @@
 
 namespace App\Controllers;
 
-use App\Controllers\BaseController;
-use CodeIgniter\HTTP\ResponseInterface;
-
 class TalkController extends BaseController
 {
     public function index()
     {
-        //
+    }
+
+    public function word($word)
+    {
+        echo 'Palavra -> '.$word;
+    }
+
+    public function words($value1, $value2)
+    {
+        echo $value1;
+        echo '<br>';
+        echo $value2;
+    }
+
+    public function other($value1 = 'Felipe')
+    {
+        echo $value1;
     }
 }
