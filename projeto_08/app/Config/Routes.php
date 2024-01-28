22127da2c2f796302849c69485ebe17ab47df854
Felipe Pinheiro <santospinheiro627@gmail.com>
2024-01-27 21:26:15 -0300

    create routes and pages

--- projeto_08/app/Config/Routes.php
+++ projeto_08/app/Config/Routes.php
@@ -6,3 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'MainController::index');
+$routes->get('/products', 'MainController::products');
+$routes->get('/where_we_are', 'MainController::where_we_are');
--- projeto_08/app/Controllers/MainController.php
+++ projeto_08/app/Controllers/MainController.php
@@ -4,8 +4,21 @@ namespace App\Controllers;
 
 class MainController extends BaseController
 {
+    // return view with title
     public function index()
     {
-        return view('home');
+        return view('home', ['title' => 'CiGBurguer']);
+    }
+
+    // return view with title
+    public function products()
+    {
+        return view('products', ['title' => 'Products']);
+    }
+
+    // return view with title
+    public function where_we_are()
+    {
+        return view('products', ['title' => 'Where We Are']);
     }
 }
