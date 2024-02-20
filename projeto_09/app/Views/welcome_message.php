8ee2f5faa46aa3bbf282aa768fa47e451a83b9c8
Felipe Santos <santospinheiro627@gmail.com>
2024-02-20 14:04:20 -0300

    up

--- projeto_09/app/Views/welcome_message.php
+++ projeto_09/app/Views/welcome_message.php
@@ -1,14 +1,20 @@
 <!DOCTYPE html>
+
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="<?php echo base_url('assets/css/app.css'); ?>" rel="stylesheet">
-    <title>tailwind css</title>
+    <title><?php echo $title; ?></title>
 </head>
+
 <body>
-    <p class="text-3xl bg-blue-500 text-center text-greeb-600 block">
+    
+    <p class="block text-3xl text-center bg-blue-500 text-greeb-600">
         Olá mundo
     </p>
+
 </body>
+
 </html>
